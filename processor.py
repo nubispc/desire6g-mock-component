84acf34f7ac42447ee2c456f59f07d7e7aed952b
ale161 <ale.black161@gmail.com>
2024-02-27 12:02:12 +0200

    Update

--- processor.py
+++ processor.py
@@ -1,12 +1,13 @@
 import os
+import asyncio
 from ProcessingSystems import processor_rabbitmq, processor_kafka
 
 # Get the messaging system from environment variable
 messaging_system = os.getenv("MESSAGING_SYSTEM", "rabbitmq")
 
 if messaging_system.lower() == "rabbitmq":
-    processor_rabbitmq.main_loop()
+    asyncio.run(processor_rabbitmq.main_loop())  # Await the main_loop coroutine
 elif messaging_system.lower() == "kafka":
-    processor_kafka.main_loop()
+    asyncio.run(processor_kafka.main_loop())  # Await the main_loop coroutine
 else:
     raise ValueError("Invalid messaging system. Please specify either 'rabbitmq' or 'kafka'.")
